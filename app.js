89da73817ca358b4c00547be8a3322f3ed1f740b
hjk0795 <hjk0795@gmail.com>
2022-09-27 14:24:19 +1300

    Procfile added

--- app.js
+++ app.js
@@ -132,6 +132,6 @@ app.get("/:customListName", function(req, res) {
   
 })
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
